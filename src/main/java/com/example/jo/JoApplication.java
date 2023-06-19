5b996bd374a623f843de39410a4cdfb87d5a373c
pascal <dupascal00@gmail.com>
2023-06-19 11:43:43 +0200

    init folder meteo

--- src/main/java/com/example/jo/JoApplication.java
+++ src/main/java/com/example/jo/JoApplication.java
@@ -2,12 +2,25 @@ package com.example.jo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class JoApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(JoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public List<String> bonjour(){
+        return List.of("Bonjour", "pas","de","pblm ");
     }
 
 }
