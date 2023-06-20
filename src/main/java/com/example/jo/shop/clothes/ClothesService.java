23eb6af9fc4d544b5df30f7da543cf91cb574e55
williammadie <william.madie0@gmail.com>
2023-06-20 18:35:50 +0200

    add Shop (Clothes and Accessories)

---
+++ src/main/java/com/example/jo/shop/clothes/ClothesService.java
@@ -0,0 +1,46 @@
+package com.example.jo.shop.clothes;
+
+import com.example.jo.shop.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClothesService implements ShopService<ClothesDto> {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Product addNewProduct(ClothesDto newClothes) {
+        Product newProduct = new Clothes(newClothes.getModelName(), newClothes.getPrice(), newClothes.getQty(), newClothes.getType());
+        jdbcTemplate.update(
+                "INSERT INTO jo.public.accessoire (name, price, qty, section) VALUES (?, ?, ?, ?)",
+                newClothes.getModelName(),
+                newClothes.getPrice(),
+                newClothes.getQty(),
+                newClothes.getType()
+        );
+        return newProduct;
+    }
+
+    @Override
+    public Product decreaseProductQty(String productId) {
+        Product product = getProductWithId(productId);
+        int newQuantity = product.getQty() - 1;
+        jdbcTemplate.update("UPDATE vetement SET qty = ? where id = ?", newQuantity, Integer.parseInt(productId));
+        product.setQty(newQuantity);
+        return product;
+    }
+
+    @Override
+    public List<Product> listAvailableProducts() {
+        return jdbcTemplate.query("SELECT * FROM vetement WHERE qty > 0", new ClothesMapper());
+    }
+
+    @Override
+    public Product getProductWithId(String id) {
+        return jdbcTemplate.query("SELECT * FROM vetement WHERE id = ?", new ClothesMapper(), Integer.parseInt(id)).get(0);
+    }
+}
